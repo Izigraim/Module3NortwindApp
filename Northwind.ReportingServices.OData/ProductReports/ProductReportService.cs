7f23b0ca236f92f8e92535a58d9c4992ed16c6ce
Izigraim <z7635332@gmail.com>
2020-02-23 15:16:15 +0300

    Getting data for all product.

---
+++ Northwind.ReportingServices.OData/ProductReports/ProductReportService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Services.Client;
+using System.Linq;
+using System.Threading.Tasks;
+using NorthwindProduct = NorthwindModel.Product;
+
+namespace Northwind.ReportingServices.OData.ProductReports
+{
+    /// <summary>
+    /// Represents a service that produces product-related reports.
+    /// </summary>
+    public class ProductReportService
+    {
+        private readonly NorthwindModel.NorthwindEntities entities;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProductReportService"/> class.
+        /// </summary>
+        /// <param name="northwindServiceUri">An URL to Northwind OData service.</param>
+        public ProductReportService(Uri northwindServiceUri)
+        {
+            this.entities = new NorthwindModel.NorthwindEntities(northwindServiceUri ?? throw new ArgumentNullException(nameof(northwindServiceUri)));
+        }
+
+        /// <summary>
+        /// Gets a product report with all current products.
+        /// </summary>
+        /// <returns>Returns <see cref="ProductReport{T}"/>.</returns>
+        public async Task<ProductReport<ProductPrice>> GetCurrentProductsReport()
+        {
+            var query = (DataServiceQuery<NorthwindProduct>)this.entities.Products;
+
+            var result = await Task<IEnumerable<NorthwindProduct>>.Factory.FromAsync(query.BeginExecute(null, null), (ar) =>
+            {
+                return query.EndExecute(ar);
+            }); 
+
+            var productPrices = new List<ProductPrice>();
+            foreach (var product in result)
+            {
+                productPrices.Add(new ProductPrice
+                {
+                    Name = product.ProductName,
+                    Price = product.UnitPrice ?? 0,
+                });
+            }
+
+            return new ProductReport<ProductPrice>(productPrices);
+        }
+
+        /// <summary>
+        /// Gets a product report with most expensive products.
+        /// </summary>
+        /// <param name="count">Items count.</param>
+        /// <returns>Returns <see cref="ProductReport{ProductPrice}"/>.</returns>
+        public async Task<ProductReport<ProductPrice>> GetMostExpensiveProductsReport(int count)
+        {
+            return new ProductReport<ProductPrice>(Array.Empty<ProductPrice>());
+        }
+    }
+}
